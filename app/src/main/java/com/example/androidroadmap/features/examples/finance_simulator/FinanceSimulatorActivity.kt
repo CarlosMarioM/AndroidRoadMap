005ff70a27f865cdc831b350c875924f0ab6e6f1
Mario Mederos <carlosmariomederos1@gmail.com>
2026-02-03 19:26:40 -0500

    feature - Create Finance Simulator Screen and Activity

---
+++ app/src/main/java/com/example/androidroadmap/features/examples/finance_simulator/FinanceSimulatorActivity.kt
@@ -0,0 +1,10 @@
+package com.example.androidroadmap.features.examples.finance_simulator
+
+import androidx.activity.ComponentActivity
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FinanceSimulatorActivity : ComponentActivity() {
+
+}
---
+++ app/src/main/java/com/example/androidroadmap/features/examples/finance_simulator/FinanceSimulatorScreen.kt
@@ -0,0 +1,14 @@
+package com.example.androidroadmap.features.examples.finance_simulator
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun FinanceSimulatorScreen(){
+
+}
+
+@Preview
+@Composable
+fun FinanceSimulatorScreenPreview() = FinanceSimulatorScreen()
